4be56bad62033acec7130ad62fa8ca37aca31d9f
sophwang0228 <sophielizwang-27@peddie.org>
2026-02-18 17:44:07 -0500

    Update Autonomous.java

--- src/main/java/frc/robot/Autonomous.java
+++ src/main/java/frc/robot/Autonomous.java
@@ -326,14 +326,14 @@ public class Autonomous {
             new AutoDriveCommand(
                 List.of(
                     new Pose2d(1.400, 5.950, Rotation2d.fromDegrees(0)),
-                    new Pose2d(1.801, 3.071, Rotation2d.fromDegrees(-100.222)),
+                    new Pose2d(2, 3.071, Rotation2d.fromDegrees(-100.222)),
                     new Pose2d(0.95, 0.613, Rotation2d.fromDegrees(180))
                     
                 ),
                 new PathConstraints(0.5, 0.5, 3 * Math.PI, 4 * Math.PI),
 
                 //new PathConstraints(1, 1, 3 * Math.PI, 4* Math.PI),
-                new IdealStartingState(0, Rotation2d.fromDegrees(180)),
+                new IdealStartingState(0, Rotation2d.fromDegrees(0)),
                 new GoalEndState(0, Rotation2d.fromDegrees(180))
             ),
             new WaitCommand(2),
@@ -354,7 +354,7 @@ public class Autonomous {
                 
                 new PathConstraints(0.5, 0.5, 3 * Math.PI, 4 * Math.PI),
 
-                new IdealStartingState(0, Rotation2d.fromDegrees(92.651)),
+                new IdealStartingState(0, Rotation2d.fromDegrees(180)),
                 new GoalEndState(0, Rotation2d.fromDegrees(105.255))
 
             ),
